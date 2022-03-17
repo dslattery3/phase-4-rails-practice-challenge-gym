16a1c7e0424956ffb30731ed8fe756627fb364db
Dalton <slattery.da@gmail.com>
2022-03-17 08:56:45 -0500

    idk

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,7 +5,17 @@ class Membership < ApplicationRecord
   validates :charge, presence: true
   validate :gym_id, :gym_relationship
 
+  private
 
+  def gym_relationship
+      # client = Client.find(self.client_id)
+      # gym = Gym.find(self.gym_id)
+      # if Gym.find_by(client: client.id)
+      #   errors.add(:client_id, "not unique relationship")
+      # end
+
+
+  end
 
 
 
